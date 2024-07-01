1949e6b3ddebe517ffc6a5bc373c32f4a4b836cb
M-Ajay-Yadav <majay805@gmail.com>
2024-07-01 14:14:51 +0530

    Their was error while first calculation which is fixed

--- src/App.jsx
+++ src/App.jsx
@@ -4,202 +4,201 @@ import OperationButton from "./OperationButton";
 import "./style.css";
 // import Routes from ".";
 
+export const ACTIONS = {
+  ADD_DIGIT: "add-digit",
+  CHOOSE_OPERATION: "choose-operation",
+  CLEAR: "clear",
+  DELETE_DIGIT: "delete-digit",
+  EVALUATE: "evaluate",
+};
 
-
-export const ACTIONS ={
-  ADD_DIGIT:'add-digit',
-  CHOOSE_OPERATION: 'choose-operation',
-  CLEAR:'clear',
-  DELETE_DIGIT:'delete-digit',
-  EVALUATE:'evaluate'
-}
-
-function reducer(state, { type, payload }){
-  
+function reducer(state, { type, payload }) {
   switch (type) {
     case ACTIONS.ADD_DIGIT:
-      if(state.overwrite){
-        return{
+      if (state.overwrite) {
+        return {
           ...state,
           currentOperand: payload.digit,
           overwrite: false,
-        }
+        };
       }
       if (payload.digit === "0" && state.currentOperand === "0") {
-        return state
+        return state;
       }
-      if (payload.digit === "." && state.currentOperand.includes('.')) {
-        return state
+      if (payload.digit === "." && state.currentOperand.includes(".")) {
+        return state;
       }
       return {
         ...state,
         currentOperand: `${state.currentOperand || ""}${payload.digit}`,
       };
-      case ACTIONS.CHOOSE_OPERATION:
-        if(state.currentOperand == null && state.previousOperand == null) {
-          return state
-        }
+    case ACTIONS.CHOOSE_OPERATION:
+      if (state.currentOperand == null && state.previousOperand == null) {
+        return state;
+      }
 
-        if (state.currentOperand ==null){
-          return{
-            ...state,
-            operation: payload.operation,
-          }
-        }
-
-        if (state.previousOperand == null) {
-          return{
-            ...state,
-            operation: payload.operation,
-            previousOperand: state.currentOperand,
-            currentOperand: null,
-          }
-        }
-        // break;
-
+      if (state.currentOperand == null) {
         return {
           ...state,
-          previousOperand: evaluate(state),
           operation: payload.operation,
-          currentOperand: null
+        };
+      }
 
-        }
-      case ACTIONS.CLEAR:
-        return {};
-      
-      case ACTIONS.DELETE_DIGIT:
-        if ( state.overwrite){
-          return{
-            ...state,
-            overwrite: false,
-            currentOperand: null
-          }
-        }
-        if (state.currentOperand == null) return state
-        if ( state.currentOperand.length === 1) {
-          return{ ...state, currentOOperand: null }
-        }
-
+      if (state.previousOperand == null) {
         return {
           ...state,
-          currentOperand: state.currentOperand.slice(0, -1)
-        }
-      case ACTIONS.EVALUATE:
-        if(
-          state.operation == null ||
-          state.currentOperand == null ||
-          state.previousOperand == null
-        ){
-          return state
-        }
-
-        return{
-          ...state,
-          overwrite: true, 
-          previousOperand: null,
-          operation: null,
-          currentOperand: evaluate(state)
+          operation: payload.operation,
+          previousOperand: state.currentOperand,
+          currentOperand: null,
         };
+      }
+      // break;
 
-        default:
-          // Handle unexpected or unsupported types
-          console.error("Unsupported type: " + type);
-          return state; // or handle it as needed
-    
+      return {
+        ...state,
+        previousOperand: evaluate(state),
+        operation: payload.operation,
+        currentOperand: null,
+      };
+    case ACTIONS.CLEAR:
+      return {};
+
+    case ACTIONS.DELETE_DIGIT:
+      if (state.overwrite) {
+        return {
+          ...state,
+          overwrite: false,
+          currentOperand: null,
+        };
+      }
+      if (state.currentOperand == null) return state;
+      if (state.currentOperand.length === 1) {
+        return { ...state, currentOOperand: null };
+      }
+
+      return {
+        ...state,
+        currentOperand: state.currentOperand.slice(0, -1),
+      };
+    case ACTIONS.EVALUATE:
+      if (
+        state.operation == null ||
+        state.currentOperand == null ||
+        state.previousOperand == null
+      ) {
+        return state;
+      }
+
+      return {
+        ...state,
+        overwrite: true,
+        previousOperand: null,
+        operation: null,
+        currentOperand: evaluate(state),
+      };
+
+    default:
+      // Handle unexpected or unsupported types
+      console.error("Unsupported type: " + type);
+      return state; // or handle it as needed
   }
 }
 
-function evaluate({ currentOperand, previousOperand, operation}){
-  const prev = parseFloat(previousOperand)
-  const current = parseFloat(currentOperand)
-  if (isNaN(prev) || isNaN(current)) return ""
-  let computation = ""
-  switch (operation){
+function evaluate({ currentOperand, previousOperand, operation }) {
+  const prev = parseFloat(previousOperand);
+  const current = parseFloat(currentOperand);
+  if (isNaN(prev) || isNaN(current)) return "";
+  let computation = "";
+  switch (operation) {
     case "+":
-      computation = prev + current
-      break
+      computation = prev + current;
+      break;
     case "-":
-      computation = prev - current
-      break
+      computation = prev - current;
+      break;
     case "*":
-      computation = prev * current
-      break
+      computation = prev * current;
+      break;
     case "÷":
-      computation = prev / current
-      break
-     default:
-        // Handle unexpected or unsupported operations
-        console.error("Unsupported operation: " + operation);
-        computation = ""; // or handle it as needed
-        break;
-    
+      computation = prev / current;
+      break;
+    default:
+      // Handle unexpected or unsupported operations
+      console.error("Unsupported operation: " + operation);
+      computation = ""; // or handle it as needed
+      break;
   }
 
-  return computation.toString()
-} 
+  return computation.toString();
+}
 
-const INTEGER_FORMATTER = new Intl.NumberFormat("en-us",{
-  maximumFractionDigits:0,
-})
-function formatOperand(operand){
-  if(operand == null)  return
-  const [integer,decimal]=operand.split(".")
-  if(decimal == null) return INTEGER_FORMATTER.format(integer)
-  return `${INTEGER_FORMATTER.format(integer)}.${decimal}`
+const INTEGER_FORMATTER = new Intl.NumberFormat("en-us", {
+  maximumFractionDigits: 0,
+});
+function formatOperand(operand) {
+  if (operand == null) return;
+  const [integer, decimal] = operand.split(".");
+  if (decimal == null) return INTEGER_FORMATTER.format(integer);
+  return `${INTEGER_FORMATTER.format(integer)}.${decimal}`;
 }
 
 function App() {
-  
-  const [{currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer, {
-    currentOperand: "",
-    previousOperand: "",
-    operation: "",
-    overwrite: false,
-  })
+  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
+    reducer,
+    {
+      currentOperand: null,
+      previousOperand: null,
+      operation: null,
+      overwrite: false,
+    }
+  );
 
   // dispatch({ type:ACTIONS.ADD_DIGIT,payload: { digit: 1 }})
-  return(
-    <div className="calculator-grid" >
+  return (
+    <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand">{previousOperand} {operation}</div>
+        <div className="previous-operand">
+          {previousOperand} {operation}
+        </div>
         <div className="current-Operand">{formatOperand(currentOperand)}</div>
       </div>
 
-    <button className="span-two" onClick={() => dispatch( {type: ACTIONS.CLEAR})}>
-      AC
+      <button
+        className="span-two"
+        onClick={() => dispatch({ type: ACTIONS.CLEAR })}
+      >
+        AC
       </button>
-    <button
-    onClick= {() => dispatch({type:ACTIONS.DELETE_DIGIT})}>
-      DEL
+      <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>
+        DEL
       </button>
-    <OperationButton operation="÷" dispatch={dispatch} />
+      <OperationButton operation="÷" dispatch={dispatch} />
 
-    <DigitButton digit="1" dispatch={dispatch} />
-    <DigitButton digit="2" dispatch={dispatch} />
-    <DigitButton digit="3" dispatch={dispatch} />
-    <OperationButton operation="*" dispatch={dispatch} />
+      <DigitButton digit="1" dispatch={dispatch} />
+      <DigitButton digit="2" dispatch={dispatch} />
+      <DigitButton digit="3" dispatch={dispatch} />
+      <OperationButton operation="*" dispatch={dispatch} />
 
-    <DigitButton digit="4" dispatch={dispatch} />
-    <DigitButton digit="5" dispatch={dispatch} />
-    <DigitButton digit="6" dispatch={dispatch} />
-    <OperationButton operation="+" dispatch={dispatch} />
+      <DigitButton digit="4" dispatch={dispatch} />
+      <DigitButton digit="5" dispatch={dispatch} />
+      <DigitButton digit="6" dispatch={dispatch} />
+      <OperationButton operation="+" dispatch={dispatch} />
 
-    <DigitButton digit="7" dispatch={dispatch} />
-    <DigitButton digit="8" dispatch={dispatch} />
-    <DigitButton digit="9" dispatch={dispatch} />
-    <OperationButton operation="-" dispatch={dispatch} />
+      <DigitButton digit="7" dispatch={dispatch} />
+      <DigitButton digit="8" dispatch={dispatch} />
+      <DigitButton digit="9" dispatch={dispatch} />
+      <OperationButton operation="-" dispatch={dispatch} />
 
-    <button>.</button>
-    <DigitButton digit="0" dispatch={dispatch} />
-    <button 
-      className="span-two" 
-      onClick={() => dispatch({ type: ACTIONS.EVALUATE })}
-    
-    >
-      =
+      <button>.</button>
+      <DigitButton digit="0" dispatch={dispatch} />
+      <button
+        className="span-two"
+        onClick={() => dispatch({ type: ACTIONS.EVALUATE })}
+      >
+        =
       </button>
     </div>
-  )
+  );
 }
 
 export default App;
